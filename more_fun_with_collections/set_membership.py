1e88106a1e3e809eabd7492e67476d838057d2f8
DanielBMeeker <54420579+DanielBMeeker@users.noreply.github.com>
2020-06-23 12:50:55 -0500

    Tests passed, comments added

--- more_fun_with_collections/set_membership.py
+++ more_fun_with_collections/set_membership.py
@@ -1,2 +1,23 @@
+"""
+Program: set_membership.py
+Author: Daniel Meeker
+Date: 6/23/2020
+
+This program defines a function - in_set() to find out
+if a value is in a set or not.
+"""
+
+
 def in_set(a_set, value):
-    pass
+    """
+    This function takes in a set and a value to determine
+    if the value is in the set.
+
+    :param a_set: A set to be searched
+    :param value: The value being searched for in the set
+    :return: Boolean True or False
+    """
+    if value in a_set:
+        return True
+    else:
+        return False
--- test/test_set_membership.py
+++ test/test_set_membership.py
@@ -1,3 +1,11 @@
+"""
+Program: test_set_membership.py
+Author: Daniel Meeker
+Date: 6/23/2020
+
+This program defines a function - in_set() to find out
+if a value is in a set or not. This file tests the function.
+"""
 import unittest
 from more_fun_with_collections import set_membership as sm
 a_set = {1, 2, 3, 4, 5}
