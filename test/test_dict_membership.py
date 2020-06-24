5d142c8df48e8f43972eea4b1899dcf6c2c45320
DanielBMeeker <54420579+DanielBMeeker@users.noreply.github.com>
2020-06-24 14:42:15 -0500

    Basic Dictionary initial commit

---
+++ test/test_dict_membership.py
@@ -0,0 +1,14 @@
+import unittest
+from more_fun_with_collections import dict_membership as dm
+
+
+class MyTestCase(unittest.TestCase):
+    def test_in_dict_true(self):
+        self.assertTrue()
+
+    def test_in_dict_false(self):
+        self.assertFalse()
+
+
+if __name__ == '__main__':
+    unittest.main()
