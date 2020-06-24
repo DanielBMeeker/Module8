905521e5b530fb210c7fbf6feb4ce91484e0664e
DanielBMeeker <54420579+DanielBMeeker@users.noreply.github.com>
2020-06-24 14:51:22 -0500

    Tests passed, comments added

--- more_fun_with_collections/dict_membership.py
+++ more_fun_with_collections/dict_membership.py
@@ -1,2 +1,24 @@
-def in_dict():
+"""
+Program: dict_membership.py
+Author: Daniel Meeker
+Date: 6/24/2020
+
+This program defines and tests a function that will determine
+if an element is in a dictionary or not.
+"""
+
+
+def in_dict(a_dict, key):
+    """
+    This function is going to take in a dictionary and a key
+    to determine if the key is in the dictionary.
+
+    :param a_dict: A dictionary to be searched
+    :param key: A key being searched for in the dictionary
+    :return: a boolean value for if the key is in the dictionary
+    """
+    return key in a_dict
+
+
+if __name__ == '__main__':
     pass
--- test/test_dict_membership.py
+++ test/test_dict_membership.py
@@ -1,13 +1,27 @@
+"""
+Program: test_dict_membership.py
+Author: Daniel Meeker
+Date: 6/24/2020
+
+This program defines and tests a function that will determine
+if an element is in a dictionary or not. This file tests the
+function.
+"""
+
 import unittest
 from more_fun_with_collections import dict_membership as dm
+
+a_dict = {'A': 90, 'B': 80, 'C': 70, 'D': 60, 'F': 0}
+key_in_dict = 'A'
+key_not_in_dict = 'S'
 
 
 class MyTestCase(unittest.TestCase):
     def test_in_dict_true(self):
-        self.assertTrue()
+        self.assertTrue(dm.in_dict(a_dict, key_in_dict))
 
     def test_in_dict_false(self):
-        self.assertFalse()
+        self.assertFalse(dm.in_dict(a_dict, key_not_in_dict))
 
 
 if __name__ == '__main__':
