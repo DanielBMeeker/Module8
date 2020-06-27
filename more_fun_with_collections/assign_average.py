cd4230f5f33ccc481ae31d43964f183d9d21105d
DanielBMeeker <54420579+DanielBMeeker@users.noreply.github.com>
2020-06-27 14:15:38 -0500

    passed first set of tests

--- more_fun_with_collections/assign_average.py
+++ more_fun_with_collections/assign_average.py
@@ -1,2 +1,14 @@
-def switch_average():
-    pass
+def switch_average(a_dict, key):
+    """
+    This function takes in a dictionary and performs a
+    switch selection on the dictionary using a key. If the
+    key is not in the dictionary it will raise a ValueError.
+    This function allows for a slight typo from where it is
+    called by always capitalizing the key before using it.
+
+    :param a_dict: a dictionary to be searched
+    :param key: a key used to sort the dictionary
+    :return: the value of the key
+    """
+    if str(key).capitalize() == 'A':
+        return a_dict.get(str(key).capitalize())
--- test/test_assign_average.py
+++ test/test_assign_average.py
@@ -1,31 +1,33 @@
 import unittest
 from more_fun_with_collections import assign_average as aa
 
+a_dict = {'A': 90, 'B': 80, 'C': 70, 'D': 60, 'F': 0}
+
 
 class MyTestCase(unittest.TestCase):
     def test_switch_average_A(self):
-        self.assertEqual(90, aa.switch_average('A'))
-        self.assertEqual(90, aa.switch_average('a'))
+        self.assertEqual(90, aa.switch_average(a_dict, 'A'))
+        self.assertEqual(90, aa.switch_average(a_dict, 'a'))
 
     def test_switch_average_B(self):
-        self.assertEqual(80, aa.switch_average('B'))
-        self.assertEqual(80, aa.switch_average('b'))
+        self.assertEqual(80, aa.switch_average(a_dict, 'B'))
+        self.assertEqual(80, aa.switch_average(a_dict, 'b'))
 
     def test_switch_average_C(self):
-        self.assertEqual(70, aa.switch_average('C'))
-        self.assertEqual(70, aa.switch_average('c'))
+        self.assertEqual(70, aa.switch_average(a_dict, 'C'))
+        self.assertEqual(70, aa.switch_average(a_dict, 'c'))
 
     def test_switch_average_D(self):
-        self.assertEqual(60, aa.switch_average('D'))
-        self.assertEqual(60, aa.switch_average('d'))
+        self.assertEqual(60, aa.switch_average(a_dict, 'D'))
+        self.assertEqual(60, aa.switch_average(a_dict, 'd'))
 
     def test_switch_average_F(self):
-        self.assertEqual(0, aa.switch_average('F'))
-        self.assertEqual(0, aa.switch_average('f'))
+        self.assertEqual(0, aa.switch_average(a_dict, 'F'))
+        self.assertEqual(0, aa.switch_average(a_dict, 'f'))
 
     def test_switch_average_non_key(self):
         self.assertRaises(ValueError)
-        aa.switch_average('S')
+        aa.switch_average(a_dict, 'S')
 
 
 if __name__ == '__main__':
