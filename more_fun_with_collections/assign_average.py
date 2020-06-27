5fc4fff3eeeac1c51fb6b2eed392fb1d351f0a12
DanielBMeeker <54420579+DanielBMeeker@users.noreply.github.com>
2020-06-27 14:19:27 -0500

    passed fourth set of tests

--- more_fun_with_collections/assign_average.py
+++ more_fun_with_collections/assign_average.py
@@ -16,3 +16,5 @@ def switch_average(a_dict, key):
         return a_dict.get(str(key).capitalize())
     elif str(key).capitalize() == 'C':
         return a_dict.get(str(key).capitalize())
+    elif str(key).capitalize() == 'D':
+        return a_dict.get(str(key).capitalize())
