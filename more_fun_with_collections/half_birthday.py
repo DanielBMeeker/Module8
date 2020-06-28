77d0d0eda9cb85165128bbade00a73e62fe792d6
DanielBMeeker <54420579+DanielBMeeker@users.noreply.github.com>
2020-06-28 14:14:15 -0500

    half_birthday complete

--- more_fun_with_collections/half_birthday.py
+++ more_fun_with_collections/half_birthday.py
@@ -1,2 +1,24 @@
+"""
+Program: half_birthday.py
+Author: Daniel Meeker
+Date: 6/28/2020
+
+This program defines a half birthday function that calculates
+the half birthday of a given birthday.
+"""
+import datetime
+from datetime import timedelta
+
+
 def half_birthday(birthday_datetime):
-    pass
+    """
+    This function calculates your half-birthday using timedelta
+    with the weeks argument.
+    :param birthday_datetime: a datetime representing birthday
+    :return: the datetime of the half birthday
+    """
+    half_b_day = (datetime.date(birthday_datetime.year,
+                                birthday_datetime.month,
+                                birthday_datetime.day)
+                  + datetime.timedelta(weeks=26))
+    return half_b_day.isoformat()
--- test/test_half_birthday.py
+++ test/test_half_birthday.py
@@ -1,3 +1,11 @@
+"""
+Program: half_birthday.py
+Author: Daniel Meeker
+Date: 6/28/2020
+
+This program defines a half birthday function that calculates
+the half birthday of a given birthday. This file tests it.
+"""
 import unittest
 import datetime
 from more_fun_with_collections import half_birthday as hb
@@ -6,7 +14,8 @@ from more_fun_with_collections import half_birthday as hb
 class MyTestCase(unittest.TestCase):
     def test_half_birthday(self):
         birthday = datetime.datetime(2020, 10, 7)
-        self.assertEqual((2021, 4, 7), hb.half_birthday(birthday))
+        expected = datetime.date(2021, 4, 7).isoformat()
+        self.assertEqual(expected, hb.half_birthday(birthday))
 
 
 if __name__ == '__main__':
